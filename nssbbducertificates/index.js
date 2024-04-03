dcd777d79cbc367f69c5bafb02c2ab18b39558b7
mrymf111 <mariyamfatima077@gmail.com>
2024-04-03 23:08:37 +0530

    first commit

---
+++ nssbbducertificates/index.js
@@ -0,0 +1,710 @@
+
+var blood = [
+"9026568054",
+"9555867997",
+"8948515918",
+"9140247615",
+"8810824142",
+"6388636736",
+"7390081130",
+"9115247511",
+"6393293381",
+"6306284218",
+"9369985828",
+"6394990798",
+"8960055357",
+"8004080382",
+"7754081002",
+"9140431055",
+"8933050070",
+"9369210188",
+"9580496023",
+"9555876251",
+"7081289773",
+"7236035459",
+"9708153594",
+"9336248929",
+"9305651970",
+"6395980363",
+"7054162834",
+"8528992012",
+"9717166569",
+"9368114901",
+"8299507908",
+"8577001721",
+"8318968233",
+"8299593865",
+"8887807456",
+"8368899572",
+"9517229493",
+"8318968233",
+"7380358207",
+"7781890843",
+"6386134620",
+"9336709002"
+];
+
+var bloodName = [
+"Rudraksh Rai",
+"Khushi Mehrotra",
+"Yasmeen",
+"Prakhar Verma",
+"Aryan Verma",
+"Yuvraj Singh", 
+"Abbas Mehdi Hasan", 
+"Shikhar Upadhyay",
+"Anuj Singh",
+"Abhinav joshi", 
+"Anishka Gupta",
+"Rishabh dwivedi", 
+"Shivang Mishra",
+"Kashik Mishra",
+"Adarsh gupta",
+"Manish Verma",
+"Purandev Singh",
+"Abhinav Pandey",
+"Aditya Nikhil Raj",
+"Arpita Singh",
+"Annat Tripathi",
+"Anushka Shukla",
+"Avanish dubey",
+"Ayan Ahmad",
+"Manjeet Gupta",
+"Sumit Yadav",
+"Jai bhargava", 
+"Mohd. Sami",
+"Shruti Shukla",
+"Pallavi Raturi",
+"Abhishek Kumar pandey",
+"Mohd Faiz Qureshi",
+"Ali Ahmed",
+"Aditya Raj",
+"Ayush Kathri",
+"Shubham Tripathi",
+"Mohd Adnan",
+"Ali Ahmad",
+"Shadab Ansari",
+"Md Danish",
+"Mohd Faiz Qureshi",
+"Mariyam Fatima Rizvi"  
+];
+
+// poster Numbers and names
+
+
+var poster = [
+"6397063216",
+"9120953242",
+"8931980269",
+"9140853269",
+"6306538382",
+"7781890843",
+"7267906352",
+"7620720947",
+"9648367868",
+"7275170307",
+"6397063216",
+"8726398174",
+"7651978103",
+"8057886511",
+"8528728766",
+"8532998088",
+"8368899572",
+"9517229493",
+"8318968233",
+"7380358207",
+"6386134620",
+"9336709002",
+"8874962113",
+"9555288026",
+"9142264877"
+]
+
+var posterName =[
+"Vidyank Verma",
+"Aditi Singh", 
+"Anu Kushwaha", 
+"Arpita Singh", 
+"Deepshika singh",
+"Md Danish", 
+"Nandini Singh", 
+"NIKITA SINGH",
+"Pratibha Umrao", 
+"Priya Singh", 
+"Sanjana Singh",
+"Shivani Yadav",
+"Sumi Gaur",
+"Tanu Tiwari",
+"Vaishnavi Agarwal", 
+"Yashika singh tomar",
+"Shubham Tripathi",
+"Mohd Adnan",
+"Ali Ahmad",
+"Shadab Ansari",
+"Mohd Faiz Qureshi",
+"Mariyam Fatima Rizvi",
+"Yazdaan Butool",
+"Khushboo Singh",
+"Sinu Kumari" 
+];
+
+// Speech name and Number
+
+var speech= [
+"9101824025",
+"9415175584",
+"8601028300",
+"7458048119",
+"9508863897",
+"8103139145",
+"9651425978",
+"6397063216",
+"9793048868",
+"8604449052",
+"9115247511",
+"8879367682",
+"8081662051",
+"6306286303",
+"7307906942",
+"9369985828",
+"8368899572",
+"9517229493",
+"8318968233",
+"7380358207",
+"7781890843",
+"6386134620",
+"7905690450",
+"9336709002",
+"8874962113"
+];
+
+var speechName = [
+"Rishav Mishra", 
+"Shivansh Chandra",
+"Om Hari Singh yadav",
+"Ayush Pathak",
+"Sonali Kumari", 
+"Khushi Singh", 
+"Ananya Singhal",
+"Krishna Singh", 
+"Deeksha Bhadauria", 
+"Mohd Farhan", 
+"Shikhar Upadhyay", 
+"Agrima Dwivedi", 
+"Khushi yadav",
+"Dolly Shahi", 
+"Sakshi Mishra", 
+"Anishka Gupta",
+"Shubham Tripathi",
+"Mohd Adnan",
+"Ali Ahmad",
+"Shadab Ansari",
+"Md Danish",
+"Mohd Faiz Qureshi",
+"Ananya Verma",
+"Mariyam Fatima Rizvi",
+"Yazdaan Butool"
+];
+
+// Digital Poster Making
+
+digitalPoster = [
+"2647AK",
+"9987AS",
+"3592DG",
+"5942YS",
+"4325AS",
+"8509AP",
+"4748AV",
+"9354AK",
+"7103AK",
+"4763AS",
+"9633GM",
+"3003KS",
+"9809RM",
+"4840AS",
+"2927AS",
+"5810SS",
+"4026AK",
+"9945RS",
+"6375RU",
+"4402UG",
+"4059AS",
+"7820JK",
+"1558LG",
+"4852HG",
+"5691DS",
+"1462SM",
+"4825KS",
+"4854AC",
+"7717ST"
+];
+
+digitalPosterName = [
+  "Abhishek Kumar Yadav",
+  "Adhiraj Singh",
+  "Dheeraj Gupta",
+  "Yash Chawla",
+  "Adarsh Shukla", 
+  "Ankit Pandey",
+  "Arpit Verma", 
+  "Aman Kr.Singh",
+  "Ayush kr.Gupta",
+  "Anshuman Singh", 
+  "Garvit Mittal",
+  "Krish Gupta",
+  "Rishabh Maddy", 
+  "Adarsh Srivastava", 
+  "Akshat Srivastava", 
+  "Sawan Singh",
+  "Aditya kr. Srivastava", 
+  "Rohan Singh", 
+  "Rishav Upadhyay", 
+  "Vijay Raj Gupta", 
+  "Akarsh Srivastava", 
+  "Jitendra Kumar Verma",
+  "Lalit Gangwar",
+  "Himanshu Gupta",
+  "Disha Singh", 
+  "Shruti Mishra", 
+  "Kirtika Singh",
+  "Anshuman Chaturvedi", 
+  "Sidhant Tiwari"
+];
+
+// election speech competition
+
+electionSpeech = [
+  "9336709002",
+  "8881473233",
+  "8917845002"
+];
+
+electionSpeechName = [
+  "Mariyam Fatima Rizvi",
+  "Aapa",
+  "Neelank Rao"
+];
+
+
+
+//function for blood
+const inputValBlood = document.querySelector("#name-b");
+const submit_btn_b = document.querySelector("#submit-b");
+submit_btn_b.addEventListener("click",()=>{
+    let val = inputValBlood.value;
+    // val = val.trim();
+    let roll = prompt("Enter Your Phone No: ");
+    
+    let isValid = false;   
+    for(let i=0; i<blood.length; i++){
+      if(blood[i] == roll && bloodName[i].toLowerCase() == val.toLowerCase()){
+        isValid = true;
+        break;
+      }        
+    }
+    if(isValid == false){
+      alert("You are not a valid person, please fill name which you had provided at the time of Registration... ");
+    }else{
+      generatePDFBlood(val);
+      alert(val+" , your blood donation certificate is generated successfully...");
+    }
+
+  
+});
+
+const generatePDFBlood = async (name) =>{ 
+  const {PDFDocument, StandardFonts, rgb} = PDFLib;
+  name = name.toUpperCase();
+
+  const exBytes = await fetch("./Bld_dn_camp.pdf").then((res) =>{
+      return res.arrayBuffer();
+  });
+
+  const pdfDoc = await PDFDocument.load(exBytes);
+  const timesRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman);
+  
+  // Get the first page of the document
+  const pages = pdfDoc.getPages();
+  const firstPage = pages[0];
+
+  
+  // Draw a string of text diagonally across the first page 
+  
+  // xPos ---> line se pahle ki 
+  // line ki width 19 charachters
+  // 1 character --- 15px 
+  let xPos = 170;
+  let nameLenth = name.length;
+  let offset = (490 - nameLenth*15)/2;
+  firstPage.drawText(name, {
+  x: xPos + offset,
+  y: 240,
+  size:25,
+  // color: rgb(0, 0.53, 0.71),
+  // color: rgb(0, 0, 0),
+  font:timesRomanFont,
+  // color: rgb(250, 250, 250),
+});
+
+const uri = await pdfDoc.saveAsBase64({dataUri:true});
+// document.querySelector("#mypdf").src = uri;
+
+  // const pdfDataUri = await pdfDoc.saveAsBase64({ dataUri: true });
+  // document.getElementById("mypdf").src = pdfDataUri;
+
+  // Serialize the PDFDocument to bytes (a Uint8Array)
+const pdfBytes = await pdfDoc.save();
+console.log("Done creating");
+
+// this was for creating uri and showing in iframe
+
+// const pdfDataUri = await pdfDoc.saveAsBase64({ dataUri: true });
+// document.getElementById("pdf").src = pdfDataUri;
+
+var file = new File(
+  [pdfBytes],
+  "NSS Blood Donation Certificate.pdf",
+  {
+    type: "application/pdf;charset=utf-8",
+  }
+);
+saveAs(file);
+};
+
+
+
+// function for Poster
+const inputValPoster = document.querySelector("#name-p");
+const submit_btn_p = document.querySelector("#submit-p");
+submit_btn_p.addEventListener("click",()=>{
+    let val = inputValPoster.value;
+    let key = prompt("Enter Your Phone No: ");
+    let isValid = false;   
+    for(let i=0; i<poster.length; i++){
+      if(poster[i] == key && posterName[i].toLowerCase() == val.toLowerCase()){
+        isValid = true;
+        break;
+      }        
+    }
+    if(isValid == false){
+      alert("You are not a valid person..");
+    }else{
+      generatePDFPoster(val);
+      alert(val+" , your certification for poster making competition is generated successfully...");
+    }  
+});
+
+const generatePDFPoster = async (name) =>{ 
+  const {PDFDocument, StandardFonts, rgb} = PDFLib;
+  name = name.toUpperCase();
+
+  const exBytes = await fetch("./poster_making.pdf").then((res) =>{
+      return res.arrayBuffer();
+  });
+
+  const pdfDoc = await PDFDocument.load(exBytes);
+  const timesRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman);
+  
+  // Get the first page of the document
+  const pages = pdfDoc.getPages();
+  const firstPage = pages[0];
+
+  
+  // Draw a string of text diagonally across the first page 
+  
+  // xPos ---> line se pahle ki 
+  // line ki width 19 charachters
+  // 1 character --- 15px 
+  let xPos = 170;
+  let nameLenth = name.length;
+  let offset = (490 - nameLenth*15)/2;
+  firstPage.drawText(name, {
+  x: xPos + offset,
+  y: 240,
+  size:25,
+  // color: rgb(0, 0.53, 0.71),
+  // color: rgb(0, 0, 0),
+  font:timesRomanFont,
+  // color: rgb(250, 250, 250),
+});
+
+const uri = await pdfDoc.saveAsBase64({dataUri:true});
+// document.querySelector("#mypdf").src = uri;
+
+  // const pdfDataUri = await pdfDoc.saveAsBase64({ dataUri: true });
+  // document.getElementById("mypdf").src = pdfDataUri;
+
+  // Serialize the PDFDocument to bytes (a Uint8Array)
+const pdfBytes = await pdfDoc.save();
+console.log("Done creating");
+
+// this was for creating uri and showing in iframe
+
+// const pdfDataUri = await pdfDoc.saveAsBase64({ dataUri: true });
+// document.getElementById("pdf").src = pdfDataUri;
+
+var file = new File(
+  [pdfBytes],
+  "NSS Poster Making Participation Certificate.pdf",
+  {
+    type: "application/pdf;charset=utf-8",
+  }
+);
+saveAs(file);
+};
+
+
+// function for speech
+const submit_btn_s = document.querySelector("#submit-s");
+const inputValSpeech = document.querySelector("#name-s");
+submit_btn_s.addEventListener("click",()=>{
+  let val = inputValSpeech.value;
+  let roll = prompt("Enter Your Phone No: ");
+    let isValid = false;   
+    for(let i=0; i<speech.length; i++){
+      if(speech[i] == roll && speechName[i].toLowerCase() == val.toLowerCase()){
+        isValid = true;
+        break;
+      }        
+    }
+    if(isValid == false){
+      alert("You are not a valid person..");
+    }else{
+      generatePDFSpeech(val);
+      alert(val+" , your speech competition certificate is generated successfully...");
+    }
+});
+
+const generatePDFSpeech = async (name) =>{ 
+  const {PDFDocument, StandardFonts, rgb} = PDFLib;
+  name = name.toUpperCase();
+
+  const exBytes = await fetch("./Speech_comp.pdf").then((res) =>{
+      return res.arrayBuffer();
+  });
+
+  const pdfDoc = await PDFDocument.load(exBytes);
+  const timesRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman);
+  
+  // Get the first page of the document
+  const pages = pdfDoc.getPages();
+  const firstPage = pages[0];
+
+  
+  // Draw a string of text diagonally across the first page 
+  
+  // xPos ---> line se pahle ki 
+  // line ki width 19 charachters
+  // 1 character --- 15px 
+  let xPos = 170;
+  let nameLenth = name.length;
+  let offset = (490 - nameLenth*15)/2;
+  firstPage.drawText(name, {
+  x: xPos + offset,
+  y: 240,
+  size:25,
+  // color: rgb(0, 0.53, 0.71),
+  // color: rgb(0, 0, 0),
+  font:timesRomanFont,
+  // color: rgb(250, 250, 250),
+});
+
+const uri = await pdfDoc.saveAsBase64({dataUri:true});
+// document.querySelector("#mypdf").src = uri;
+
+  // const pdfDataUri = await pdfDoc.saveAsBase64({ dataUri: true });
+  // document.getElementById("mypdf").src = pdfDataUri;
+
+  // Serialize the PDFDocument to bytes (a Uint8Array)
+const pdfBytes = await pdfDoc.save();
+console.log("Done creating");
+
+// this was for creating uri and showing in iframe
+
+// const pdfDataUri = await pdfDoc.saveAsBase64({ dataUri: true });
+// document.getElementById("pdf").src = pdfDataUri;
+
+var file = new File(
+  [pdfBytes],
+  "NSS Speech Comp Certificate.pdf",
+  {
+    type: "application/pdf;charset=utf-8",
+  }
+);
+saveAs(file);
+};
+
+
+// function for Digital Poster Making Competition
+
+const submit_btn_dp = document.querySelector("#submit-dp");
+const inputValDigitalPoster = document.querySelector("#name-dp");
+submit_btn_dp.addEventListener("click",()=>{
+  let val = inputValDigitalPoster.value.trim();
+  let indexNum = prompt("Enter Your Index Num: ");
+    let isValid = false;   
+    for(let i=0; i<digitalPoster.length; i++){
+      if(digitalPoster[i] == indexNum && digitalPosterName[i].toLowerCase() == val.toLowerCase()){
+        isValid = true;
+        break;
+      }        
+    }
+    if(isValid == false){
+      alert("You are not a valid person..");
+    }else{
+      generatePDFDigitalPoster(val);
+      alert(val+" , your digital making competition certificate is generated successfully...");
+    }
+});
+
+const generatePDFDigitalPoster = async (name) =>{ 
+  const {PDFDocument, StandardFonts, rgb} = PDFLib;
+  name = name.toUpperCase();
+
+  const exBytes = await fetch("./digital_poster.pdf").then((res) =>{
+      return res.arrayBuffer();
+  });
+
+  const pdfDoc = await PDFDocument.load(exBytes);
+  const timesRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman);
+  
+  // Get the first page of the document
+  const pages = pdfDoc.getPages();
+  const firstPage = pages[0];
+
+  
+  // Draw a string of text diagonally across the first page 
+  
+  // xPos ---> line se pahle ki 
+  // line ki width 19 charachters
+  // 1 character --- 15px 
+  let xPos = 0;
+  let nameLenth = name.length;
+  let offset = (490 - nameLenth*12)/2;
+  firstPage.drawText(name, {
+  x: xPos + offset + 5,
+  y: 280,
+  size:22,
+  // color: rgb(0, 0.53, 0.71),
+  // color: rgb(0, 0, 0),
+  font:timesRomanFont,
+  // color: rgb(250, 250, 250),
+});
+
+const uri = await pdfDoc.saveAsBase64({dataUri:true});
+// document.querySelector("#mypdf").src = uri;
+
+  // const pdfDataUri = await pdfDoc.saveAsBase64({ dataUri: true });
+  // document.getElementById("mypdf").src = pdfDataUri;
+
+  // Serialize the PDFDocument to bytes (a Uint8Array)
+const pdfBytes = await pdfDoc.save();
+console.log("Done creating");
+
+// this was for creating uri and showing in iframe
+
+// const pdfDataUri = await pdfDoc.saveAsBase64({ dataUri: true });
+// document.getElementById("pdf").src = pdfDataUri;
+
+var file = new File(
+  [pdfBytes],
+  "NSS Digital Poster Comp Certificate.pdf",
+  {
+    type: "application/pdf;charset=utf-8",
+  }
+);
+saveAs(file);
+};
+
+// function for election awareness speech competition
+
+const submit_btn_ec = document.querySelector("#submit-ec");
+const inputValElectionSpeech = document.querySelector("#name-ec");
+submit_btn_ec.addEventListener("click",()=>{
+  let val = inputValElectionSpeech.value.trim();
+  let indexNum = prompt("Enter Your Index Num: ");
+    let isValid = false;   
+    for(let i=0; i<electionSpeech.length; i++){
+      if(electionSpeech[i] == indexNum && electionSpeechName[i].toLowerCase() == val.toLowerCase()){
+        isValid = true;
+        break;
+      }        
+    }
+    if(isValid == false){
+      alert("You are not a valid person..");
+    }else{
+      generatePDFElectionSpeech(val);
+      alert(val+" , your speech on election awareness competition certificate is generated successfully...");
+    }
+});
+
+const generatePDFElectionSpeech = async (name) =>{ 
+  const {PDFDocument, StandardFonts, rgb} = PDFLib;
+  name = name.toUpperCase();
+
+  const exBytes = await fetch("./speechElection.pdf").then((res) =>{
+      return res.arrayBuffer();
+  });
+
+  const pdfDoc = await PDFDocument.load(exBytes);
+  const timesRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman);
+  
+  // Get the first page of the document
+  const pages = pdfDoc.getPages();
+  const firstPage = pages[0];
+
+  
+  // Draw a string of text diagonally across the first page 
+  
+  // xPos ---> line se pahle ki 
+  // line ki width 19 charachters
+  // 1 character --- 15px 
+  let xPos = 0;
+  let nameLenth = name.length;
+  let offset = (490 - nameLenth*12)/2;
+  firstPage.drawText(name, {
+  x: xPos + offset + 5,
+  y: 280,
+  size:22,
+  // color: rgb(0, 0.53, 0.71),
+  // color: rgb(0, 0, 0),
+  font:timesRomanFont,
+  // color: rgb(250, 250, 250),
+});
+
+const uri = await pdfDoc.saveAsBase64({dataUri:true});
+// document.querySelector("#mypdf").src = uri;
+
+  // const pdfDataUri = await pdfDoc.saveAsBase64({ dataUri: true });
+  // document.getElementById("mypdf").src = pdfDataUri;
+
+  // Serialize the PDFDocument to bytes (a Uint8Array)
+const pdfBytes = await pdfDoc.save();
+console.log("Done creating");
+
+// this was for creating uri and showing in iframe
+
+// const pdfDataUri = await pdfDoc.saveAsBase64({ dataUri: true });
+// document.getElementById("pdf").src = pdfDataUri;
+
+var file = new File(
+  [pdfBytes],
+  "NSS Election Speech Comp Certificate.pdf",
+  {
+    type: "application/pdf;charset=utf-8",
+  }
+);
+saveAs(file);
+};
+
+// generatePDFDigitalPoster("Jitendra Kumar Verma");
+// generatePDF("Shashwat Shrivastav");
+// generatePDF("Mariyam Fatima Rizvi");
+// generatePDF("Abhishek kumar pandey");
+
+
+
+
+
+
